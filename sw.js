45c4c888e381167c7920ff254aa7e34795d06af1
Davids275 <52664366+Davids275@users.noreply.github.com>
2026-08-25 15:27:09 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Guarda os arquivos locais e também as bibliotecas do CDN, então
    depois da PRIMEIRA abertura com internet o app roda offline. */
 
-const CACHE = 'lifeos-v3';
+const CACHE = 'lifeos-v5';
 const LOCAIS = [
   './',
   './index.html',
